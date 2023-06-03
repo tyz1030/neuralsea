c302f6aa6f2b0dc5d142dc42b6b64d977496b9c8
tyz <zty181030@gmail.com>
2023-06-03 16:33:45 -0400

    code cleaning

--- nerf/raysampler.py
+++ nerf/raysampler.py
@@ -5,24 +5,16 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
-from typing import List, Optional
-
+from typing import List
 import torch
-# from pytorch3d.renderer import MonteCarloRaysampler, MultinomialRaysampler, RayBundle
 from pytorch3d.renderer import RayBundle
-from .raysampling import MonteCarloRaysampler, MultinomialRaysampler
-
 from pytorch3d.renderer.cameras import CamerasBase
-from pytorch3d.renderer.implicit.sample_pdf import sample_pdf, sample_pdf_python
-import torch.nn.functional as F
 
 
 class BoundingPlaneRaysamplerCustom(torch.nn.Module):
     def __init__(
         self,
         n_pts_per_ray: int,
-        min_depth: float,
-        max_depth: float,
         n_rays_per_image: int,
         image_width: int,
         image_height: int,
@@ -52,7 +44,7 @@ class BoundingPlaneRaysamplerCustom(torch.nn.Module):
         self.image_height = image_height
         self.n_rays_per_image = n_rays_per_image
         self.n_pts_per_ray = n_pts_per_ray
-    
+
     @torch.no_grad()
     def get_n_chunks(self, chunksize: int, batch_size: int):
         return int(
@@ -70,16 +62,18 @@ class BoundingPlaneRaysamplerCustom(torch.nn.Module):
         lower = torch.cat((z_vals[..., :1], mids), dim=-1)
         # Stratified samples in those intervals.
         # Keep starting point and ending point unchanged by [1:-1]
-        z_vals[..., 1:-1] = (lower + (upper - lower) * torch.rand_like(lower))[..., 1:-1]
+        z_vals[..., 1:-1] = (lower + (upper - lower) *
+                             torch.rand_like(lower))[..., 1:-1]
         return ray_bundle._replace(lengths=z_vals)
 
     @torch.no_grad()
     def _normalize_by_z_depth(self, ray_bundle: RayBundle):
         ray_bundle = ray_bundle._replace(
-            directions=ray_bundle.directions/torch.abs(ray_bundle.directions[..., -1].unsqueeze(-1))
+            directions=ray_bundle.directions /
+            torch.abs(ray_bundle.directions[..., -1].unsqueeze(-1))
         )
         return ray_bundle
-    
+
     @torch.no_grad()
     def forward(
         self,
@@ -94,8 +88,14 @@ class BoundingPlaneRaysamplerCustom(torch.nn.Module):
 
         origin = -torch.matmul(cameras.T, torch.transpose(cameras.R[0], 0, 1))
         z_to_center = origin[0, -1]
-        depths = torch.linspace(0,1,self.n_pts_per_ray, device = cameras.device)
-        
+        depths = torch.linspace(
+            0, 1, self.n_pts_per_ray, device=cameras.device)
+
+
+        ####TODO
+
+        # Adjust the following to put the scene between near and far bounding planes
+        ####
         # z_to_foreground = -z_to_center+0.5
         # z_foreg_to_back = 0.3
 
@@ -106,44 +106,55 @@ class BoundingPlaneRaysamplerCustom(torch.nn.Module):
         if self.training:
             xys = 2*torch.rand([self.n_rays_per_image, 2])-1.0
             temp_xys = torch.concat([xys[:, 0, None], xys[:, 1, None]], dim=1)
-            xy_coords = torch.tensor([[self.image_width, self.image_height]])*temp_xys/2/cameras.focal_length.cpu()
+            xy_coords = torch.tensor(
+                [[self.image_width, self.image_height]])*temp_xys/2/cameras.focal_length.cpu()
 
-            ray_dirs_cam = torch.cat([xy_coords, torch.ones_like(temp_xys)[:, 0, None]], dim = 1)
-            ray_dirs_world = torch.matmul(ray_dirs_cam.cuda(), cameras.R[0].transpose(0, 1))
-            rays_dirs_world_normalized = ray_dirs_world/torch.abs(ray_dirs_world[:, 2])[..., None]
-            rays_zs = depths[None, None].expand(batch_size, self.n_rays_per_image, self.n_pts_per_ray)
+            ray_dirs_cam = torch.cat(
+                [xy_coords, torch.ones_like(temp_xys)[:, 0, None]], dim=1)
+            ray_dirs_world = torch.matmul(
+                ray_dirs_cam.cuda(), cameras.R[0].transpose(0, 1))
+            rays_dirs_world_normalized = ray_dirs_world / \
+                torch.abs(ray_dirs_world[:, 2])[..., None]
+            rays_zs = depths[None, None].expand(
+                batch_size, self.n_rays_per_image, self.n_pts_per_ray)
 
             ray_bundle = RayBundle(
-                    origins=origin.expand(self.n_rays_per_image, 3),
-                    directions=rays_dirs_world_normalized,
-                    lengths=rays_zs.unsqueeze(0), # 1*n_rays*n_pts*n_pts_to_lightsource
-                    xys=xys.cuda()[None, ...],
-                )
+                origins=origin.expand(self.n_rays_per_image, 3),
+                directions=rays_dirs_world_normalized,
+                # 1*n_rays*n_pts*n_pts_to_lightsource
+                lengths=rays_zs.unsqueeze(0),
+                xys=xys.cuda()[None, ...],
+            )
         else:
 
-            grid_y, grid_x = torch.meshgrid(torch.range(0.5, self.image_height-0.5), torch.range(0.5, self.image_width-0.5), indexing='ij')
-            xy_grid = torch.cat([grid_x[..., None], grid_y[..., None]], dim = -1)
-            xys = xy_grid/torch.tensor([self.image_width, self.image_height])-0.5
-            
-            xy_grid_cam = xy_grid-torch.tensor([self.image_width, self.image_height])/2
-            xy_coords = xy_grid_cam.view(self.image_width*self.image_height, 2)/cameras.focal_length.cpu()
-            
-            ray_dirs_cam = torch.cat([ xy_coords, torch.ones_like(xy_coords)[:, 0, None]], dim = 1)
-            ray_dirs_world = torch.matmul(ray_dirs_cam.cuda(), cameras.R[0].transpose(0, 1))
-            # ray_dirs_world = torch.matmul(ray_dirs_cam.cuda(), cameras.R[0])
-            rays_dirs_world_normalized = ray_dirs_world/torch.abs(ray_dirs_world[:, 2])[..., None]
-            rays_zs = depths[None, None].expand(batch_size, self.image_width*self.image_height, self.n_pts_per_ray)
+            grid_y, grid_x = torch.meshgrid(torch.range(
+                0.5, self.image_height-0.5), torch.range(0.5, self.image_width-0.5), indexing='ij')
+            xy_grid = torch.cat([grid_x[..., None], grid_y[..., None]], dim=-1)
+            xys = xy_grid / \
+                torch.tensor([self.image_width, self.image_height])-0.5
 
-            # print(ray_dirs_cam)
-            # print(ray_dirs_cam.shape)
-            # import sys
-            # sys.exit()
+            xy_grid_cam = xy_grid - \
+                torch.tensor([self.image_width, self.image_height])/2
+            xy_coords = xy_grid_cam.view(
+                self.image_width*self.image_height, 2)/cameras.focal_length.cpu()
+
+            ray_dirs_cam = torch.cat(
+                [xy_coords, torch.ones_like(xy_coords)[:, 0, None]], dim=1)
+            ray_dirs_world = torch.matmul(
+                ray_dirs_cam.cuda(), cameras.R[0].transpose(0, 1))
+            # ray_dirs_world = torch.matmul(ray_dirs_cam.cuda(), cameras.R[0])
+            rays_dirs_world_normalized = ray_dirs_world / \
+                torch.abs(ray_dirs_world[:, 2])[..., None]
+            rays_zs = depths[None, None].expand(
+                batch_size, self.image_width*self.image_height, self.n_pts_per_ray)
+
             full_ray_bundle = RayBundle(
-                    origins=origin.expand(self.image_width*self.image_height, 3)[None, ...],
-                    directions=rays_dirs_world_normalized[None, ...],
-                    lengths=rays_zs, # 1*n_rays*n_pts*n_pts_to_lightsource
-                    xys=2*xys.cuda()[None, ...],
-                )
+                origins=origin.expand(
+                    self.image_width*self.image_height, 3)[None, ...],
+                directions=rays_dirs_world_normalized[None, ...],
+                lengths=rays_zs,  # 1*n_rays*n_pts*n_pts_to_lightsource
+                xys=2*xys.cuda()[None, ...],
+            )
             # for v in full_ray_bundle:
             #     print(v.shape)
             # import sys
@@ -173,7 +184,7 @@ class BoundingPlaneRaysamplerCustom(torch.nn.Module):
                 ]
             )
         if ((self._stratified and self.training)
-            or (self._stratified_test and not self.training)):
+                or (self._stratified_test and not self.training)):
             ray_bundle = self._stratify_ray_bundle(ray_bundle)
 
         return ray_bundle
